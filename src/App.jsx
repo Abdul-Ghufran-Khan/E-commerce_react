96ed87e18ae82c1317816f289327efbead6a5db6
Abdul-Ghufran-Khan <abdulghufran267@gmail.com>
2024-09-29 18:55:18 +0500

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,32 +7,53 @@ import Cards from "./Components/Cards";
 
 function App() {
   const [products, setproducts] = useState([])
-  const [cartitems , setcartitems] = useState([])
+  const [cartitems, setcartitems] = useState([])
 
   useEffect(() => {
     fetchproducts()
   }, [])
 
+  useEffect(() => {
+    const ItemsinCart = JSON.parse(localStorage.getItem("cart"))
+    if (ItemsinCart) {
+      setcartitems([...ItemsinCart])
+    }
+  }, [])
+
+  useEffect(() => {
+    if (cartitems.length) {
+      localStorage.setItem("cart", JSON.stringify(cartitems))
+    }
+    }, [cartitems])
+
+  const addcartitem = (item) => {
+    const items = [...cartitems]
+    const itemInd = items.findIndex((data) => data.id === item.id)
+    console.log(itemInd);
+    if (itemInd == -1) {
+      items.push(item)
+      setcartitems([...items])
+    }
+  }
+
   const fetchproducts = async () => {
     const products = await Getproduct()
     setproducts([...products])
-    console.log(products);
-    
   }
 
   return (
     <>
       <div className='container fixed top-0 left-0 right-0  max-w-screen-2xl h-16 bg-stone-950'>
         <div className=" mx-10 flex justify-between items-center h-16">
-          <h1 className='text-orange-600 text-2xl p-7 uppercase '>E-Store</h1>
-          <ul className='flex gap-10 text-orange-500 cursor-pointer'>
+          <h1 className='text-orange-600 text-2xl p-7 uppercase font-bold'>E-Store</h1>
+          <ul className='flex gap-10 text-orange-500 cursor-pointer font-semibold'>
             <li className="hover:text-orange-600">Home</li>
             <li className="hover:text-orange-600">About</li>
             <li className="hover:text-orange-600">Contact</li>
           </ul>
           <div className="text-orange-500">
             <h1 className='w-3 h-3 mx-11 text-white'>{cartitems.length}</h1>
-            <FaShoppingCart className="w-14 h-6 cursor-pointer "/>
+            <FaShoppingCart className="w-14 h-6 cursor-pointer " />
           </div>
         </div>
       </div>
@@ -40,7 +61,15 @@ function App() {
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-wrap -m-4">
             {products.map((data) => {
-              return <Cards key={data.id} item={data} />
+              const isAddedtoCart = cartitems.findIndex((product) => product.id === data.id) !== -1;     
+              return(
+                <Cards
+                 Addtocart={() => addcartitem(data)} 
+                 key={data.id} 
+                 item={data} 
+                 isAddedtoCart={isAddedtoCart}
+                 />
+              );
             })}
           </div>
         </div>
--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -2,13 +2,13 @@ import { FaShoppingCart } from "react-icons/fa";
 
 
 
-export default function Cards({item}) {
+export default function Cards({item , Addtocart , isAddedtoCart}) {
 
     const { category, image, description, title , price} = item
     const descriptions = item.description.slice(0, 120)
 
     return (
-        <div className="p-4 md:w-1/3">
+        <div className="p-4 md:w-1/3 sm:w-2/3">
             <div className="h-full border-2 border-stone-900 rounded-lg overflow-hidden">
                 <img
                     className="lg:h-80 md:h-72 sm:h-72 w-full object-fill object-center"
@@ -25,15 +25,14 @@ export default function Cards({item}) {
                     <p className="leading-relaxed mb-3">
                         {descriptions+"..."}
                     </p>
-                    <div className="flex items-center flex-wrap ">
+                    <div className="flex items-center flex-wrap" onClick={Addtocart}>
                         <a className="text-orange-500 inline-flex items-center md:mb-2 lg:mb-0 cursor-pointer">
-                            Add to Cart
-                            <FaShoppingCart className="mx-2 w-4 h-4 cursor-pointer"/>
+                            {isAddedtoCart ? "Added" : `Add to Cart`}
                         </a>
+                    </div>
                         <span className="text-gray-500 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-800">
                             Price : ${price}
                         </span>
-                    </div>
                 </div>
             </div>
         </div>
